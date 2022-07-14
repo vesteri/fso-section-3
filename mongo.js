837aa556a2a887087e58dfa7afb0105a14fce525
vesteri <arttu.vesterinen@tuni.fi>
2022-07-14 15:04:19 +0300

    excercise 3.12

---
+++ mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const param = process.argv;
+if (param.length !== 3 && param.length !== 5) {
+  console.log(
+    'type "node mongo.js [password]" to list all contacts n\
+  or "node mongo.js [password] [name] [number]" to add a contact'
+  );
+  process.exit(1);
+}
+
+const password = param[2];
+
+const url = `mongodb+srv://vesteri:${password}@cluster0.rzml3.mongodb.net/personsApp?retryWrites=true&w=majority`;
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  id: Number,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+if (process.argv.length == 3) {
+  console.log('phonebook:');
+  Person.find({}).then((contacts) => {
+    contacts.forEach((person) => {
+      console.log(person.name, person.number);
+    });
+    mongoose.connection.close();
+  });
+} else {
+  const person = new Person({
+    name: param[3],
+    number: param[4],
+    id: Math.floor(Math.random() * 10000),
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
